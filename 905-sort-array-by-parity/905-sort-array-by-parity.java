e1aeead69c10868376e2f21a48234998e5812f25
Subhajit78 <110681996+Subhajit78@users.noreply.github.com>
2022-08-27 00:35:38 +0530

    Time: 2 ms (49.65%), Space: 47.9 MB (80.77%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,22 +1,38 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int ans[]= new int[nums.length];
-        int counter =0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                ans[counter]=nums[i];
-                counter++;
-            }
+//         int ans[]= new int[nums.length];
+//         int counter =0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]%2==0){
+//                 ans[counter]=nums[i];
+//                 counter++;
+//             }
                 
-        }
-          for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==1){
-                ans[counter]=nums[i];
-                counter++;
-            }
+//         }
+//           for(int i=0;i<nums.length;i++){
+//             if(nums[i]%2==1){
+//                 ans[counter]=nums[i];
+//                 counter++;
+//             }
                 
-        }
+//         }
 
-         return  ans;   
-    }
+//          return  ans;   
+        
+        
+        int i=0;
+        int j= nums.length-1;
+        while(i < j){
+            if(nums[i] % 2 > nums[j] %2 ){
+                nums[i]=nums[i] ^ nums[j];
+                nums[j]=nums[i] ^ nums[j];
+                nums[i]=nums[i] ^ nums[j];
+            }
+            
+            if(nums[i]%2==0) i++;
+            if(nums[j]%2==1) j--;
+        }
+        
+        return nums;
+      }
 }
